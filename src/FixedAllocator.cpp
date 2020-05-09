a5edd2d0f21f6172fab91cfc5d210db964657cf9
Dmitriy A <d.arbitman@gmail.com>
2020-05-09 12:34:48 -0700

    Minor refactor and comment update

--- src/FixedAllocator.cpp
+++ src/FixedAllocator.cpp
@@ -1,27 +1,26 @@
 #include "FixedAllocator.hpp"
 
 #include <cassert>
+#include <limits>
 
 namespace alloc {
 
-FixedAllocator::FixedAllocator(size_t blockSize) noexcept
-    : pAllocChunk_(nullptr), pDeallocChunk_(nullptr), blockSize_(blockSize), numBlocks_(0) {
-  assert(blockSize > 0);
+FixedAllocator::FixedAllocator(size_t block_size) noexcept
+    : p_lru_alloc_chunk_(nullptr), p_lru_dealloc_chunk_(nullptr), block_size_(block_size), num_blocks_(0) {
+  assert(block_size_ > 0);
 
-  auto numBlocks = kDefaultChunkSize / blockSize;
+  auto num_blocks = kDefaultChunkSize / block_size;
 
-  assert(numBlocks != 0);
+  assert(num_blocks != 0);
 
   // Place upper bound
-  if (numBlocks > UINT8_MAX) {
-    numBlocks = UINT8_MAX;
+  if (num_blocks > std::numeric_limits<uint8_t>::max()) {
+    num_blocks = std::numeric_limits<uint8_t>::max();
   }
 
-  numBlocks_ = static_cast<uint8_t>(numBlocks);
+  num_blocks_ = static_cast<uint8_t>(num_blocks);
 
-  assert(numBlocks_ == numBlocks);
-
-  chunks_.reserve(kDefaultVectorReservedSize);
+  chunks_.reserve(kDefaultNumberOfChunks);
 }
 
 FixedAllocator::~FixedAllocator() noexcept {
@@ -31,138 +30,144 @@ FixedAllocator::~FixedAllocator() noexcept {
 }
 
 void* FixedAllocator::Allocate() noexcept {
-  if (pAllocChunk_ == nullptr || pAllocChunk_->GetNumBlocksAvailable() == 0) {
+  if (p_lru_alloc_chunk_ == nullptr || p_lru_alloc_chunk_->GetNumBlocksAvailable() == 0) {
     // no chunk exists or no available memory in current chunk
     // Try to find one (linear search)
-    for (Chunks::iterator chunks_iter = chunks_.begin(), chunks_end = chunks_.end();; ++chunks_iter) {
+    for (Chunks::iterator chunk_iter = chunks_.begin(), chunk_iter_end = chunks_.end();; ++chunk_iter) {
       // Couldn't find a Chunk with any available space, so create a new Chunk
-      if (chunks_iter == chunks_end) {
+      if (chunk_iter == chunk_iter_end) {
         // Construct new Chunk in place at the back of the vector
-        auto& newChunk = chunks_.emplace_back();
+        auto& new_chunk = chunks_.emplace_back();
 
         // Initialize new Chunk
-        newChunk.Init(blockSize_, numBlocks_);
+        new_chunk.Init(block_size_, num_blocks_);
 
         // Update Chunk cache pointers
-        pAllocChunk_   = &newChunk;
-        pDeallocChunk_ = &newChunk;
+        p_lru_alloc_chunk_   = &new_chunk;
+        p_lru_dealloc_chunk_ = &new_chunk;
+
+        // can now exit for-loop and allocate memory
         break;
       }
 
       // Found a Chunk with available space
-      if (chunks_iter->GetNumBlocksAvailable() > 0) {
-        pAllocChunk_ = &(*chunks_iter);
+      if (chunk_iter->GetNumBlocksAvailable() > 0) {
+        p_lru_alloc_chunk_ = &(*chunk_iter);
+
+        // can now exit for-loop and allocate memory
         break;
       }
     }
   }
 
-  assert(pAllocChunk_ != nullptr);
-  assert(pAllocChunk_->GetNumBlocksAvailable() > 0);
+  assert(p_lru_alloc_chunk_ != nullptr);
+  assert(p_lru_alloc_chunk_->GetNumBlocksAvailable() > 0);
 
-  return pAllocChunk_->Allocate(blockSize_);
+  return p_lru_alloc_chunk_->Allocate(block_size_);
 }
 
-void FixedAllocator::Deallocate(void* pObject) noexcept {
+void FixedAllocator::Deallocate(void* p_object) noexcept {
   assert(!chunks_.empty());
 
-  // Linearly search for a Chunk to which pObject belongs to starting at pDeallocChunk_ and search
+  // Linearly search for a Chunk to which p_object belongs to starting at p_lru_dealloc_chunk_ and search
   // outwards
-  pDeallocChunk_ = FindChunkOwner(pObject);
+  p_lru_dealloc_chunk_ = FindChunkOwner(p_object);
 
-  assert(pDeallocChunk_ != nullptr);
+  assert(p_lru_dealloc_chunk_ != nullptr);
 
-  DoDeallocate(pObject);
+  DoDeallocate(p_object);
 }
 
-size_t FixedAllocator::getBlockSize() const noexcept { return blockSize_; }
+size_t FixedAllocator::getBlockSize() const noexcept { return block_size_; }
 
-alloc::Chunk* FixedAllocator::FindChunkOwner(void* pObject) noexcept {
+Chunk* FixedAllocator::FindChunkOwner(void* p_object) const noexcept {
   assert(!chunks_.empty());
-  assert(pDeallocChunk_ != nullptr);
+  assert(p_lru_dealloc_chunk_ != nullptr);
 
-  alloc::Chunk* pLowChunk       = pDeallocChunk_;
-  alloc::Chunk* pHighChunk      = pDeallocChunk_ + 1;
-  alloc::Chunk* pLowChunkBound  = &chunks_.front();
-  alloc::Chunk* pHighChunkBound = &chunks_.back() + 1;
+  auto* p_lo_chunk       = p_lru_dealloc_chunk_;
+  auto* p_hi_chunk       = p_lru_dealloc_chunk_ + 1;
+  auto* p_lo_chunk_bound = &chunks_.front();
+  auto* p_hi_chunk_bound = &chunks_.back() + 1;
 
-  // special case: pDeallocChunk_ is the last Chunk in the vector
-  if (pHighChunk == pHighChunkBound) {
-    pHighChunk = nullptr;
+  // special case: p_lru_dealloc_chunk_ is the last Chunk in the vector
+  if (p_hi_chunk == p_hi_chunk_bound) {
+    p_hi_chunk = nullptr;
   }
 
   // Search outward in chunks_
   for (;;) {
-    if (pLowChunk != nullptr) {
-      // pObject lies in the address space of a Chunk pointed to by pLowChunk
-      if (pLowChunk->IsInChunk(pObject)) {
-        return pLowChunk;
+    if (p_lo_chunk != nullptr) {
+      // p_object lies in the address space of a Chunk pointed to by p_lo_chunk
+      if (p_lo_chunk->IsInChunk(p_object)) {
+        return p_lo_chunk;
       }
 
-      if (pLowChunk == pLowChunkBound) {
-        pLowChunk = nullptr;
+      if (p_lo_chunk == p_lo_chunk_bound) {
+        p_lo_chunk = nullptr;
       } else {
-        --pLowChunk;
+        --p_lo_chunk;
       }
     }
 
-    if (pHighChunk != nullptr) {
-      // pObject lies in the address space of a Chunk pointed to by pHighChunk
-      if (pHighChunk->IsInChunk(pObject)) {
-        return pHighChunk;
+    if (p_hi_chunk != nullptr) {
+      // p_object lies in the address space of a Chunk pointed to by p_hi_chunk
+      if (p_hi_chunk->IsInChunk(p_object)) {
+        return p_hi_chunk;
       }
-      if (++pHighChunk == pHighChunkBound) {
-        pHighChunk = nullptr;
+      if (++p_hi_chunk == p_hi_chunk_bound) {
+        p_hi_chunk = nullptr;
       }
     }
   }
 
-  // should never reach here
+  // should never reach here because a Chunk owner MUST exist since that is the only way for a pointer to a Chunk to
+  // have been given out (ie an allocation must have happened)
+  assert(false);
   return nullptr;
 }
 
-void FixedAllocator::DoDeallocate(void* pObject) noexcept {
-  assert(pDeallocChunk_->IsInChunk(pObject));
+void FixedAllocator::DoDeallocate(void* p_object) noexcept {
+  assert(p_lru_dealloc_chunk_->IsInChunk(p_object));
 
-  pDeallocChunk_->Deallocate(pObject, blockSize_);
+  p_lru_dealloc_chunk_->Deallocate(p_object, block_size_);
 
   // Check if the Chunk that just freed memory, has no allocated blocks
   // Can potentially remove the empty Chunk object if another empty one exists
-  if (pDeallocChunk_->GetNumBlocksAvailable() == numBlocks_) {
-    // pDeallocChunk_ has all blocks freed
+  if (p_lru_dealloc_chunk_->GetNumBlocksAvailable() == num_blocks_) {
+    // p_lru_dealloc_chunk_ has all blocks freed
     // check if another chunk is empty in order to release a Chunk
-    alloc::Chunk* const pLastChunk = &chunks_.back();
+    auto* const p_last_chunk = &chunks_.back();
 
-    if (pLastChunk == pDeallocChunk_) {
-      if (chunks_.size() > 1 && pLastChunk[-1].GetNumBlocksAvailable() == numBlocks_) {
+    if (p_last_chunk == p_lru_dealloc_chunk_) {
+      if (chunks_.size() > 1 && p_last_chunk[-1].GetNumBlocksAvailable() == num_blocks_) {
         // have more than 1 Chunk object and last 2 Chunk objets are empty
         // can delete the last one
-        pLastChunk->Release();
+        p_last_chunk->Release();
         chunks_.pop_back();
-        pAllocChunk_ = pDeallocChunk_ = &chunks_.front();
+        p_lru_alloc_chunk_ = p_lru_dealloc_chunk_ = &chunks_.front();
       }
       return;
     }
 
-    // pDeallocChunk and pLastChunk are not the same Chunk
-    // checking if pLastChunk is empty
-    // if empty, release it, move pDeallocChunk_ to the back since it's also empty, then change
-    // pointer pAllocChunk to point to pDeallocChunk since it's empty and that's where free
+    // p_lru_dealloc_chunk_ and p_last_chunk are not the same Chunk
+    // checking if p_last_chunk is empty
+    // if empty, release it, move p_lru_dealloc_chunk_ to the back since it's also empty, then change
+    // pointer p_lru_alloc_chunk_ to point to p_lru_dealloc_chunk_ since it's empty and that's where free
     // blocks will be allocated from
-    if (pLastChunk->GetNumBlocksAvailable() == numBlocks_) {
-      pLastChunk->Release();
+    if (p_last_chunk->GetNumBlocksAvailable() == num_blocks_) {
+      p_last_chunk->Release();
       chunks_.pop_back();
-      std::swap(*pDeallocChunk_, chunks_.back());
-      pAllocChunk_ = pDeallocChunk_;
+      std::swap(*p_lru_dealloc_chunk_, chunks_.back());
+      p_lru_alloc_chunk_ = p_lru_dealloc_chunk_;
     }
     // if last chunk isn't empty, move empty chunk to the back
-    // set pAllocChunk to point to it since that's where free blocks should be allocated from
+    // set p_lru_alloc_chunk_ to point to it since that's where free blocks should be allocated from
     // since it can serve the most requests without searching for a free block
     else {
-      std::swap(*pDeallocChunk_, *pLastChunk);
+      std::swap(*p_lru_dealloc_chunk_, *p_last_chunk);
 
-      // pAllocChunk_ and pDeallocChunk_ now point to the same Chunk
-      pAllocChunk_ = &chunks_.back();
+      // p_lru_alloc_chunk_ and p_lru_dealloc_chunk_ now point to the same Chunk
+      p_lru_alloc_chunk_ = &chunks_.back();
     }
   }
 }
--- src/FixedAllocator.hpp
+++ src/FixedAllocator.hpp
@@ -9,11 +9,14 @@ namespace alloc {
 
 class FixedAllocator {
  public:
-  /// @brief Create a FixedAlloctor object to handle allocating memory to objects of blockSize
-  /// bytes
-  /// @param blockSize Size of objects (in bytes)
-  explicit FixedAllocator(size_t blockSize) noexcept;
+  /// @brief Create a FixedAlloctor object to handle allocating memory to objects of block_size bytes
+  /// @param block_size Size of objects (in bytes)
+  explicit FixedAllocator(size_t block_size) noexcept;
 
+  /// @brief
+  FixedAllocator(FixedAllocator&&) noexcept = default;
+
+  /// @brief
   FixedAllocator& operator=(FixedAllocator&&) noexcept = default;
 
   /// @brief release managed Chunks back to OS
@@ -23,43 +26,48 @@ class FixedAllocator {
   void* Allocate() noexcept;
 
   /// @brief Return memory back to Chunk handler
-  void Deallocate(void* pObject) noexcept;
+  /// @param p_object
+  void Deallocate(void* p_object) noexcept;
 
   /// @brief Return the size (in bytes) of the objects that this FixedAllocator allocates
   size_t getBlockSize() const noexcept;
 
   /// Deleted to prevent misuse
   FixedAllocator(const FixedAllocator&) noexcept = delete;
-  FixedAllocator(FixedAllocator&&) noexcept      = default;
   FixedAllocator& operator=(const FixedAllocator&) noexcept = delete;
 
  private:
-  using Chunks = std::vector<alloc::Chunk>;
+  using Chunks = std::vector<Chunk>;
 
   /// The default size (in bytes) of a Chunk
   static constexpr size_t kDefaultChunkSize = 262144;
 
   /// The default size (in number of Chunks) for the vector
-  static constexpr size_t kDefaultVectorReservedSize = 100;
+  static constexpr size_t kDefaultNumberOfChunks = 100;
 
-  /// @brief Find Chunk that allocated pointer pObject
-  /// @param pObject
-  alloc::Chunk* FindChunkOwner(void* pObject) noexcept;
+  /// @brief Find Chunk that allocated pointer
+  /// @param p_object Points at the block whose Chunk memory space must be found
+  /// @return Chunk* Points at the Chunk object that allocated the object ponted to by p_object
+  Chunk* FindChunkOwner(void* p_object) const noexcept;
 
-  /// @brief Perform the deallocation of pointer pObject
-  void DoDeallocate(void* pObject) noexcept;
+  /// @brief Perform the deallocation of block pointed to by p_object. The block must lie in the memory space of the
+  /// Chunk pointed to by p_lru_dealloc_chunk_
+  /// @param p_object Points at the block whose memory to free
+  void DoDeallocate(void* p_object) noexcept;
 
-  // LRU chunk that handled an alloc
-  alloc::Chunk* pAllocChunk_;
+  /// LRU chunk that handled an allococation
+  Chunk* p_lru_alloc_chunk_;
 
-  // LRU chunk that handled a dealloc
-  alloc::Chunk* pDeallocChunk_;
+  /// LRU chunk that handled a deallocation
+  Chunk* p_lru_dealloc_chunk_;
 
-  size_t blockSize_;
+  /// Size of each block (in bytes)
+  size_t block_size_;
 
-  uint8_t numBlocks_;
+  /// The number of blocks a chunk of size kDefaultChunkSize can hold
+  uint8_t num_blocks_;
 
-  // holds all chunks that handle blockSize requests
+  // holds all chunks that handle block_size_ requests
   Chunks chunks_;
 };
 
