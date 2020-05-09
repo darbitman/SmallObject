aedc8f4e8b13a77b0b217dca3e98cf06da624695
Dmitriy A <d.arbitman@gmail.com>
2020-05-09 15:55:16 -0700

    Refactor

--- src/SmallObjAllocator.cpp
+++ src/SmallObjAllocator.cpp
@@ -4,69 +4,114 @@
 #include <cassert>
 #include <iostream>
 
+namespace alloc {
+
 namespace {
 
-bool CompareFixedAllocatorSize(const alloc::FixedAllocator& FixedAllocatorObj, size_t maxObjectSize) {
-  return FixedAllocatorObj.getBlockSize() < maxObjectSize;
+bool CompareFixedAllocatorSize(const FixedAllocator& FixedAllocatorObj, size_t max_object_size) {
+  return FixedAllocatorObj.getBlockSize() < max_object_size;
 }
 
 }  // namespace
 
-namespace alloc {
+SmallObjAllocator::SmallObjAllocator(size_t max_object_size) noexcept
+    : max_object_size_(max_object_size), p_lru_allocator_(nullptr), p_lru_deallocator_(nullptr) {}
 
-SmallObjAllocator::SmallObjAllocator(size_t maxObjectSize) noexcept
-    : maxObjectSize_(maxObjectSize), pLastAlloc_(nullptr), pLastDealloc_(nullptr) {}
-
-void* SmallObjAllocator::Allocate(size_t numBytes) noexcept {
-  if (numBytes > maxObjectSize_) {
-    return operator new(numBytes);
+void* SmallObjAllocator::Allocate(size_t obj_size) noexcept {
+  if (obj_size > max_object_size_) {
+    // if obj_size isn't handle by this SmallObjAllocator, use global
+    return ::operator new(obj_size);
   }
 
-  // check if recently used FixedAllocator object services numBytes
-  // if it doesn't, search pool_ for one
-  // if one doesn't exist in pool_, create a new one
-  if (pLastAlloc_ != nullptr && pLastAlloc_->getBlockSize() == numBytes) {
-    return pLastAlloc_->Allocate();
-  }
+  /*
+    // check if recently used FixedAllocator object services obj_size
+    // if it doesn't, search pool_ for one
+    // if one doesn't exist in pool_, create a new one
+    if (p_lru_allocator_ != nullptr && p_lru_allocator_->getBlockSize() == obj_size) {
+      return p_lru_allocator_->Allocate();
+    }
 
-  // check current pool for FixedAllocator that services at least numBytes.
-  // create a new FixedAllocator if one is not found append one that handles numBytes
-  Pool::iterator iter = std::lower_bound(pool_.begin(), pool_.end(), numBytes, CompareFixedAllocatorSize);
+    // check current pool for FixedAllocator that services at least obj_size.
+    // create a new FixedAllocator if one is not found append one that handles obj_size
+    Pool::iterator iter = std::lower_bound(pool_.begin(), pool_.end(), obj_size, CompareFixedAllocatorSize);
 
-  // lower_bound may have found an object that handles allocations for objects of AT LEAST size
-  // numBytes. Check to make sure a FixedAllocator object exists that handles objects of size
-  // numBytes
-  if (iter == pool_.end() || iter->getBlockSize() != numBytes) {
-    iter          = pool_.insert(iter, alloc::FixedAllocator(numBytes));
-    pLastDealloc_ = &(pool_.back());
-  }
-  pLastAlloc_ = &(*iter);
-  return pLastAlloc_->Allocate();
+    // lower_bound may have found an object that handles allocations for objects of AT LEAST size
+    // obj_size. Check to make sure a FixedAllocator object exists that handles objects of size
+    // obj_size
+    if (iter == pool_.end() || iter->getBlockSize() != obj_size) {
+      iter               = pool_.insert(iter, FixedAllocator(obj_size));
+      p_lru_deallocator_ = &(pool_.back());
+    }
+    p_lru_allocator_ = &(*iter);
+    */
+  p_lru_allocator_ = FindAllocator(obj_size);
+  return p_lru_allocator_->Allocate();
 }
 
-void SmallObjAllocator::Deallocate(void* pObjectToDealloc, size_t numBytes) noexcept {
-  if (numBytes > maxObjectSize_) {
-    operator delete(pObjectToDealloc);
+void SmallObjAllocator::Deallocate(void* p_object_to_dealloc, size_t obj_size) noexcept {
+  if (obj_size > max_object_size_) {
+    ::operator delete(p_object_to_dealloc);
     return;
   }
 
-  // check if recently used FixedAllocator object services numBytes
+  /*
+    // check if recently used FixedAllocator object services obj_size
+    // if it doesn't, search pool_ for one
+    if (p_lru_deallocator_ && p_lru_deallocator_->getBlockSize() == obj_size) {
+      p_lru_deallocator_->Deallocate(p_object_to_dealloc);
+    }
+
+    // check current pool for FixedAllocator that services obj_size
+    Pool::iterator iter = std::lower_bound(pool_.begin(), pool_.end(), obj_size, CompareFixedAllocatorSize);
+
+    // assert instead of an if because the if is in the Allocate method
+    // It wouldn't make sense to create a new FixedAllocator object to handle a deallocation when it
+    // didn't handle the allocation
+    assert(iter != pool_.end());
+    assert(iter->getBlockSize() == obj_size);
+
+    p_lru_deallocator_ = &(*iter);
+    */
+  p_lru_deallocator_ = FindDeallocator(p_object_to_dealloc, obj_size);
+  p_lru_deallocator_->Deallocate(p_object_to_dealloc);
+}
+
+FixedAllocator* SmallObjAllocator::FindAllocator(size_t obj_size) noexcept {
+  // check if recently used FixedAllocator object services obj_size
   // if it doesn't, search pool_ for one
-  if (pLastDealloc_ && pLastDealloc_->getBlockSize() == numBytes) {
-    pLastDealloc_->Deallocate(pObjectToDealloc);
+  // if one doesn't exist in pool_, create a new one
+  if (p_lru_allocator_ != nullptr && p_lru_allocator_->getBlockSize() == obj_size) {
+    return p_lru_allocator_;
   }
 
-  // check current pool for FixedAllocator that services numBytes
-  Pool::iterator iter = std::lower_bound(pool_.begin(), pool_.end(), numBytes, CompareFixedAllocatorSize);
+  // check current pool for FixedAllocator that services objects of at least obj_size
+  // create a new FixedAllocator if one is not found append one that handles obj_size
+  const auto allocator = std::lower_bound(pool_.begin(), pool_.end(), obj_size, CompareFixedAllocatorSize);
+  if (allocator != pool_.end() || allocator->getBlockSize() != obj_size) {
+    const auto new_allocator = pool_.insert(allocator, FixedAllocator(obj_size));
 
-  // assert instead of an if because the if is in the Allocate method
-  // It wouldn't make sense to create a new FixedAllocator object to handle a deallocation when it
-  // didn't handle the allocation
-  assert(iter != pool_.end());
-  assert(iter->getBlockSize() == numBytes);
+    // update since old pointer may be invalid due to container changing size
+    p_lru_deallocator_ = &(pool_.back());
 
-  pLastDealloc_ = &(*iter);
-  pLastDealloc_->Deallocate(pObjectToDealloc);
+    return &(*new_allocator);
+  } else {
+    return &(*allocator);
+  }
+}
+
+FixedAllocator* SmallObjAllocator::FindDeallocator(void* p_object, size_t obj_size) noexcept {
+  // check if recently used FixedAllocator object services obj_size
+  // if it doesn't, search pool_ for one
+  if (p_lru_deallocator_ && p_lru_deallocator_->getBlockSize() == obj_size) {
+    p_lru_deallocator_->Deallocate(p_object);
+  }
+
+  // check current pool for FixedAllocator that services obj_size
+  const auto deallocator = std::lower_bound(pool_.begin(), pool_.end(), obj_size, CompareFixedAllocatorSize);
+
+  // guaranteed that a FixedAllocator object is found, because an object had to have existed to handle the allocation
+
+  return &(*deallocator);
 }
 
 }  // namespace alloc
--- src/SmallObjAllocator.hpp
+++ src/SmallObjAllocator.hpp
@@ -10,33 +10,46 @@ namespace alloc {
 class SmallObjAllocator {
  public:
   /// @brief create a SmallObjAllocator to handle allocations up to a maximum size (in bytes)
-  /// @param maxObjectSize The maximum size (in bytes) of objects to allocate
-  explicit SmallObjAllocator(size_t maxObjectSize) noexcept;
+  /// @param max_object_size Largest object size that this SmallObjAllocator allocates memory for (in bytes)
+  explicit SmallObjAllocator(size_t max_object_size) noexcept;
 
+  /// @brief destructor Cleans up memory
   ~SmallObjAllocator() noexcept = default;
 
   /// @brief allocate memory
-  /// @param numBytes The size (in bytes) of the object to allocate memory for
-  void* Allocate(size_t numBytes) noexcept;
+  /// @param obj_size Size of the object to allocate memory for (in bytes)
+  /// @return void* Points to a memory block of size obj_size
+  void* Allocate(size_t obj_size) noexcept;
 
-  /// deallocate memory at pointer p
-  void Deallocate(void* pObjectToDealloc, size_t numBytes) noexcept;
+  /// @brief deallocate memory pointed to by p_object_to_dealloc
+  /// @param p_object_to_dealloc Points to the object whose memory to deallocate
+  /// @param obj_size Size of the object (in bytes)
+  void Deallocate(void* p_object_to_dealloc, size_t obj_size) noexcept;
 
  private:
   using Pool = std::vector<FixedAllocator>;
 
+  /// @brief Find a FixedAllocator object that handles memory allocations for objects of size obj_size. If a new
+  /// FixedAllocator was created to allocate objects of size obj_size, then p_lru_deallocator_ gets updated since pool_
+  /// may have been modified.
+  /// @param obj_size Size of the object (in bytes)
+  /// @return FixedAllocator* Pointer to a FixedAllocator that handles allocating memory for objects of size obj_size
+  FixedAllocator* FindAllocator(size_t obj_size) noexcept;
+
+  FixedAllocator* FindDeallocator(void* p_object, size_t obj_size) noexcept;
+
   // max num of bytes handled
-  size_t maxObjectSize_;
-
-  // LRU FixedAllocator
-  FixedAllocator* pLastAlloc_;
-
-  // LRU FIxedAllocator
-  FixedAllocator* pLastDealloc_;
+  const size_t max_object_size_;
 
   // holds pool of FixedAllocator objects that handle various sized requests
   // Sorted by the size of the objects each FixedAllocator object handles
   Pool pool_;
+
+  // LRU FixedAllocator
+  FixedAllocator* p_lru_allocator_;
+
+  // LRU FIxedAllocator
+  FixedAllocator* p_lru_deallocator_;
 };
 
 }  // namespace alloc
