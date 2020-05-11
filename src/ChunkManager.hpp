2fa334612f98d5cdb799af91f675a9ea12bf72ed
Dmitriy A <d.arbitman@gmail.com>
2020-05-10 20:03:28 -0700

    Update comments

--- src/ChunkManager.hpp
+++ src/ChunkManager.hpp
@@ -6,16 +6,26 @@ namespace alloc {
 
 class ChunkManager {
  public:
-  /// @brief
+  /// @brief Construct an object that will manage Chunks that allocate blocks of size block_size (in bytes)
   /// @param block_size The size of blocks (in bytes) allocated by managed Chunks
   explicit ChunkManager(size_t block_size) noexcept;
 
+  /// @brief Get the block size (in bytes) that managed Chunks allocate
+  /// @return size_t Block size (in bytes)
   size_t GetBlockSize() const noexcept;
 
+  /// @brief Get the number of available blocks per Chunk that hasn't allocated any blocks
+  /// @return uint8_t
   uint8_t GetNumBlocks() const noexcept;
 
+  /// @brief Search for a Chunk with an available block. If one is not found, a new Chunk is created. It is possible for
+  /// all of the client's references and pointers to Chunk objects to be invalidated after this method returns.
+  /// @return Chunk& A Chunk object that has at least one free block to allocate
   Chunk& FindFreeChunk() noexcept;
 
+  /// @brief Creates a new Chunk object. It is possible for all of the client's references and pointers to Chunk objects
+  /// to be invalidated after this method returns.
+  /// @return Chunk& Reference to the newly created Chunk object
   Chunk& CreateNewChunk() noexcept;
 
   /// @brief Finds the Chunk that performed a memory allocation for a block pointed to by p_block. This Chunk is one
